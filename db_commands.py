a19336b35a32e09eccd7dedc08d41d1e9b53ba84
WHug0 <61158178+WHug0@users.noreply.github.com>
2021-10-09 21:55:54 +0200

    Update db_commands.py

--- db_commands.py
+++ db_commands.py
@@ -4,11 +4,11 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'sql11.freemysqlhosting.net'
-app.config['MYSQL_USER'] = 'sql11427664'
-app.config['MYSQL_PASSWORD'] = '1wMlmK2mBF'
-app.config['MYSQL_DB'] = 'sql11427664'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
+app.config['MYSQL_CURSORCLASS'] = ''
 
 mysql = MySQL(app)
 
